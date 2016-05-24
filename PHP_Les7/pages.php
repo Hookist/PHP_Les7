cc02100ff6b35c8c78d438a80d3297c85678e335
Hookist <spartanstas@gmail.com>
2016-05-25 01:02:30 +0300

    Add files via upload

---
+++ PHP_Les7/pages.php
@@ -0,0 +1,19 @@
+<?php
+
+    $pages = array(
+        array("id"=>1,
+            "name" => "Kontakt",
+              "link" => "kontakt.php"),
+        array("id"=>2,
+            "name" => "About",
+              "link" => "about.php"));
+
+  $mainPage = array(
+        "name" => "MainForm",
+                "link" => "index2.php");
+  $loginPage = array(
+      "name" => "LoginForm",
+      "link" => "login.php"
+      );
+      
+?>
