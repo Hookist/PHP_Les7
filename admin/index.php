bcbbece4de8fe97d11e6bac15887fc14605cbbff
Hookist <spartanstas@gmail.com>
2016-06-01 00:57:40 +0300

    Add files via upload

--- admin/index.php
+++ admin/index.php
@@ -7,6 +7,7 @@
  //    die();
  //}
 
+ $menu = getMenu();
 
     if($_SERVER['REQUEST_METHOD'] == 'POST')
     {
@@ -16,10 +17,15 @@
             addMenuItem($_POST);
             break;
             case "delete" :
-            deleteMenuItem();
+            //$_POST['id'] = $("#menu_to_delete option:selected").text();
+            $_POST['id'] = $_POST['menu_to_delete']; 
+            deleteMenuItem($_POST);
             break;
             case "edit" :
-            editMenuItem();
+            $_POST['id'] = $_POST['menu_to_edit'];
+            $_POST['name'] = $_POST['newname'];
+            $_POST['link'] = $_POST['newlink'];     
+            editMenuItem($_POST);
             break;
         }
     }
@@ -28,6 +34,16 @@
 
 
     <html>
+        <head>
+                   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+       
+
+        <link href="mystylesheet.css" rel="stylesheet">
+            
+
+        </head>
     
 <ul class="nav nav-tabs">
       <li class="active"><a data-toggle="tab" href="#add">Add</a></li>
@@ -38,25 +54,55 @@
    
    <div class="tab-content">
        
-        </div>
-        <form action="" method='post' id="add">
+        
+         <form action="" method='post' id="add" class="tab-pane fade in active">
             <input type="text" name="name" required>
             <input type="text" name="link" required>
             <input type="hidden" name="action" value="add">
             <input type="submit">
         </form>
+       
         
-        <form action="" method='post' id="delete">
-            <select name="menu_to_delete">
+       
+        <form action="" method='post' id="delete" class="tab-pane fade">
+            <select  name="menu_to_delete">
                <option selected>choose the menu</option>
-               <?php foreach($menu as $m):?>
-                <option value="<?=$m['id']?>"> <?=$m['name']?></option>
+               <?php foreach($menu as $m): ?>
+                <option value="<?= $m[0]?>"> <?=$m[1]?></option>
                 
                <?php endforeach;?>
-   
+              
             </select>
-            <input type="hidden" value="delete">
-            <input type="sumbit" value="delete">
+             <input type="hidden" name="action" value="delete">
+            <input type="submit">
         </form>
+      
+        
+       
+       
+        <form action="" method='post' id="edit" class="tab-pane fade">
+            <select name="menu_to_edit">
+               <option selected>choose the menu</option>
+               <?php foreach($menu as $m): ?>
+                <option value="<?= $m[0]?>"> <?=$m[1]?></option>
+               
+               <?php endforeach;?>
+          
+              
+              
+            </select>
+              new name : <input type="text" name="newname" required>
+                new link : <input type="text" name="newlink" required>
+            
+             <input type="hidden" name="action" value="edit">
+            <input type="submit">
+        </form>
+       
+    
+      
+            
+        
+        
+        
         
     </html>
--- function.php
+++ function.php
@@ -3,10 +3,7 @@
 
     function CheckLogin($login)
     {
-        $link = mysqli_connect("127.0.0.1","root", "", "mydb");
-
-        
-        $res = mysqli_query($link, "SELECT `UserName` FROM `User` WHERE `UserName`= '$login';");
+        $res = doQuery("SELECT `UserName` FROM `User` WHERE `UserName`= '$login';");
 
         var_dump((mysqli_num_rows($res)));
         $count = (mysqli_num_rows($res));
@@ -66,19 +63,21 @@ VALUES ('$username', '$password');");
 
     function getMenu()
     {
-         $link = mysqli_connect("127.0.0.1","root", "", "mydb");
-         $res = mysqli_query($link, "SELECT * FROM `page`;");
+       
+         $res = doQuery("SELECT * FROM `pages`;");
          if($res != false)
          {
-             $arr = mysqli_fetch_all($res, MYSQLI_ASSOC);
+             echo "zachlo";
+             var_dump($res);
+             $arr = mysqli_fetch_all($res, MYSQLI_NUM);
+             var_dump($arr);
              return $arr;
          }
     }
 
     function getPageById($id)
     {
-         $link = mysqli_connect("127.0.0.1","root", "", "mydb");
-         $res = mysqli_query($link, "SELECT * FROM `page` WHERE `id` = '$id';");
+         $res = doQuery("SELECT * FROM `pages` WHERE `id` = '$id';");
          if($res != false)
          {
              $arr = mysqli_fetch_all($res, MYSQLI_ASSOC);
@@ -88,12 +87,32 @@ VALUES ('$username', '$password');");
     
     function addMenuItem($post)
     {
-        $link = mysqli_connect("127.0.0.1", "root", "", "mydb");
-        
-        $q = mysqli_query($link, "INSERT INTO `page`(`name`, `link`)  values('${post['name']}', '${post['link']}')");
-         return $q;
+        return doQuery("INSERT INTO `pages`(`name`, `link`)  values('${post['name']}', '${post['link']}')");
     }
        
+    function deleteMenuItem($post)
+    {
+        
+       return doQuery("DELETE FROM `pages`
+                                WHERE `id` = '".$post['id']."'; ");
+        
+    }
+
+    function editMenuItem($post)
+    {
+        return doQuery("UPDATE `pages`
+                        SET `name` = '".$post['name']."', `link` = '".$post['link']."'
+                        WHERE `id` = '".$post['id']."'; ");
+        
+    }
+
+    function doQuery($query)
+    {
+         $link = mysqli_connect("127.0.0.1", "root", "", "mydb");
+        
+        $q = mysqli_query($link, $query);
+         return $q;
+    }
     
     
 ?>
